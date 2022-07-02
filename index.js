effc1ec293c89b2bb95c3560980f5a954b308828
Gor <58237309+Cactus322@users.noreply.github.com>
2022-07-03 00:04:22 +0300

    test

--- index.js
+++ index.js
@@ -107,6 +107,12 @@ app.use(express.json())
 //     next()
 // }
 
+app.get('/', (request, response) => {
+    response.send(`
+        <h1>Hello world</h1>
+    `)
+})
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);

4dde0cf9eb9f719a741bf9b6b66859d2920bee74
Gor <58237309+Cactus322@users.noreply.github.com>
2022-07-04 20:27:39 +0300

    added mongo.js

---
+++ mongo.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+    console.log(
+        "Please provide the password as an argument: node mongo.js <password>"
+    );
+    process.exit(1);
+}
+
+const password = process.argv[2];
+
+console.log(password);
+
+const url = `mongodb+srv://MyMongo:${password}@cluster0.nfqes.mongodb.net/personApp?retryWrites=true&w=majority`;
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+});
+
+const Persone = mongoose.model("Person", personSchema);
+
+const getPersonsList = () => {
+    mongoose
+    .connect(url)
+    .then(() => {
+        Persone.find({}).then(result => {
+            result.forEach(persone => {
+                console.log(persone);
+            })
+        
+            mongoose.connection.close()
+        })
+    })
+    .catch((err) => console.log(err));
+}
+
+const setPersonInfo = (name, number) => {
+    mongoose
+    .connect(url)
+    .then(() => {
+        console.log("connected");
+
+        const persone = new Persone({
+            name: name,
+            number: number,
+        });
+
+        return persone.save();
+    })
+    .then(() => {
+        console.log("note saved!");
+        return mongoose.connection.close();
+    })
+    .catch((err) => console.log(err));
+}
+
+if (process.argv.length === 5) {
+    setPersonInfo(process.argv[3], process.argv[4])
+} else {
+    getPersonsList();
+}
